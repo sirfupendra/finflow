7ee71f1576d67bd9d26527a62acdb8cb40576932
upendra sisodiya <sirfupendra@gmail.com>
2025-05-09 12:31:00 +0530

    configured socket in merchantserver

--- Finflow/backend/merchantbank/routes/merchant.routes.js
+++ Finflow/backend/merchantbank/routes/merchant.routes.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
+const MerchantController = require('../controllers/merchant.controller');
 
+router.post('register',MerchantController.registerMerchant);
--- Finflow/backend/merchantserver/merchantserver.js
+++ Finflow/backend/merchantserver/merchantserver.js
@@ -1,5 +1,9 @@
 const express=require('express');
 const app=express();
+const http=require('http');
+const cors=require('cors');
+const server=http.createServer(app);
+const {Server} = require('socket.io');
 
 const mongoose=require('mongoose');
  main = async () => {
@@ -7,13 +11,29 @@ const mongoose=require('mongoose');
     console.log('Database Connected');
 }
 main().catch(err => console.log(err));
-
-
+const io = new Server(server, {
+    cors: {
+        origin: '*', // Allow all origins (adjust for security in production)
+        methods: ['GET', 'POST'], // Allowed HTTP methods
+    },
+});
+io.on('connection',(socket)=>{
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg);
+        socket.emit('message', 'Hello from server');
+    }
+    );
+});
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send('hello from merchant server');
 });
 
-app.listen(3015,()=>{
+server.listen(3015,()=>{
     console.log(' app Server is running on port 3015');
 });
